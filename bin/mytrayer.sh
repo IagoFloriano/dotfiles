354af0c35f37cf7d104b64d15556333b2bb55212
Iago Floriano <iagomfloriano@gmail.com>
2021-10-05 03:30:03 -0300

    Script to run trayer in a specific way

---
+++ bin/mytrayer.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+trayer --align right --edge top --tint 0x282a36 --height 19 --widthtype request --transparent true --alpha 0 --SetPartialStrut true --SetDockType true --expand true --monitor 1
