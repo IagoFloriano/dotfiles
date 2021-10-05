92a679fd93063805e30fb25d560d658ac753b4e8
Iago Floriano <iagomfloriano@gmail.com>
2021-10-05 03:49:02 -0300

    patched dmenu

---
+++ suckless/mydmenu/config.def.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"hack:size=11"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#f8f8f2", "#282a36" },
+	[SchemeSel] = { "#f8f8f2", "#bd93f9" },
+  [SchemeSelHighlight] = { "#f8f8f2", "#282a36" },
+  [SchemeNormHighlight] = { "#f8f8f2", "#44475a" },
+	[SchemeOut] = { "#000000", "#000000" },
+  [SchemeMid] = { "#f8f8f2", "#6272a4" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 2;
