2b99caeeaa1947328d1e70e7e65b7907984e6950
Iago Floriano <iagomfloriano@gmail.com>
2025-03-29 17:31:03 -0300

    Add git blame keybind

--- .config/nvim/lua/yv/lazy/init.lua
+++ .config/nvim/lua/yv/lazy/init.lua
@@ -19,10 +19,4 @@ return {
       vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
     end,
   },
-  {
-    "tpope/vim-fugitive",
-    config = function ()
-      vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-    end,
-  },
 }
